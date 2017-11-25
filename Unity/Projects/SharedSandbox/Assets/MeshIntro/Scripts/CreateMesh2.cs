8ac9a6529c2512e40f2a8e6fc7bf9c7dad2ec4c0
Mohamadalchawa <ucqbma9@ucl.ac.uk>
2017-11-25 15:10:54 +0000

    2D mesh grid

---
+++ Unity/Projects/SharedSandbox/Assets/MeshIntro/Scripts/CreateMesh2.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMesh2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var filter = GetComponent<MeshFilter>();
+
+        filter.sharedMesh = CreateQuad();
+        //filter.sharedMesh = CreateTri();
+    }
+
+    Mesh CreateQuad()
+    {
+        Mesh mesh = new Mesh();
+
+        //create vertwx attru=ibutes
+        //positions
+        //colors
+        //colors?
+        //normals??
+        //uv coordinates
+
+
+        //1 create verstex attribute
+
+
+        Vector3[] positions = new Vector3[4];
+
+        positions[0] = new Vector3(0f, 0f, 0f);
+        positions[1] = new Vector3(1f, 0f, 0f);
+        positions[2] = new Vector3(0f, 1f, 0f);
+        positions[3] = new Vector3(1f, 1f, 0f);
+
+
+
+        Color[] colors = new Color[4];
+        colors[0] = new Color(1f, 0f, 0.5f, 1);
+        colors[1] = new Color(1f, 1f, 0.5f, 1);
+        colors[2] = new Color(0f, 1f, 0.5f, 1);
+        colors[3] = new Color(0f, 0f, 0.5f, 1);
+
+        //Vector3[] positions2 = new Vector3[4]
+        //{
+        //    new Vector3(0f, 0f, 0f),
+        //    new Vector3(0f, 0f, 0f),
+        //    new Vector3(0f, 0f, 0f),
+        //    new Vector3(0f, 0f, 0f),
+        //};
+        //2 create indices
+
+        int[] tris = new int[]
+        {
+            0,1,2,
+            1,3,2
+
+        };
+
+
+        //assign to mesh
+
+        mesh.vertices = positions;
+        mesh.colors = colors;
+
+        mesh.SetTriangles(tris, 0);
+
+        return mesh;
+
+
+    }
+
+    //Mesh CreateTri()
+    //{
+    //    Mesh mesh = new Mesh();
+
+    //    create vertwx attru = ibutes
+    //    positions
+    //    colors
+    //    colors?
+    //    normals ??
+    //    uv coordinates
+
+
+    //    1 create verstex attribute
+
+
+    //    Vector3[] positions = new Vector3[3];
+
+    //    positions[0] = new Vector3(0f, 0f, 0f);
+    //    positions[1] = new Vector3(1f, 0f, 0f);
+    //    positions[2] = new Vector3(0f, 1f, 0f);
+
+    //    Vector2[] texCoords = new Vector2[3];
+
+    //    texCoords[0] = new Vector2(0f, 0f);
+    //    texCoords[1] = new Vector2(1f, 0f);
+    //    texCoords[2] = new Vector2(0f, 1f);
+
+
+
+
+
+    //    Vector3[] positions2 = new Vector3[4]
+    //    {
+    //        new Vector3(0f, 0f, 0f),
+    //        new Vector3(0f, 0f, 0f),
+    //        new Vector3(0f, 0f, 0f),
+    //        new Vector3(0f, 0f, 0f),
+    //    };
+    //    2 create indices
+
+    //    int[] tris = new int[]
+    //    {
+    //        0,1,2
+
+    //    };
+
+
+    //    assign to mesh
+
+    //    mesh.vertices = positions;
+    //    mesh.uv = texCoords;
+    //    mesh.SetTriangles(tris, 0);
+
+    //    return mesh;
+
+
+    //}
+
+    ////CHALLANGE
+    //Mesh CreateTetra()
+    //{
+
+    //    Vector3[] positions = new Vector3[]
+    //    {
+    //        new Vector3(0f,0f,0f),
+    //        new Vector3(1f,0f,1f),
+    //        new Vector3(1f,1f,0f),
+    //        new Vector3(0f,1f,1f)
+
+    //    };
+
+    //    Color[] colors = new Color[]
+    //    {
+    //        new Color(0f,0f,0.5f),
+    //        new Color(1f,0f,0.5f),
+    //        new Color(0f,1f,0.5f),
+    //        new Color(1f,1f,0.5f)
+    //    };
+
+
+    //    var tris = new int[] 
+    //    {
+    //        0,1,3,
+    //        2,1,0,
+    //        2,0,3,
+    //        3,1,2 
+    //    };
+
+    //    Mesh tetra = new Mesh();
+
+    //    tetra.vertices = positions;
+    //    tetra.SetTriangles(tris,0);
+    //    tetra.colors = colors;
+    //    return tetra;
+    //}
+
+    /// <summary>
+    /// creates a colorful unit quadrilateral
+    /// </summary>
+    Mesh CreateGrid(int faceX, int faceY)
+    {
+        
+        var verts = new Vector3[]
+        {
+            
+        };
+
+
+
+
+        Mesh grid = new Mesh();
+        return grid;
+
+
+    }
+
+    
+    
+    //
+    ///
+    /// 
+    /// 
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
---
+++ Unity/Projects/SharedSandbox/Assets/MeshIntro/Scripts/CreateMesh3.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class CreateMesh3 : MonoBehaviour
+{
+    public int xSize, ySize;
+
+    private void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+	// Use this for initialization
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+    private IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh = new Mesh();
+        mesh.name = "procedural Grid";
+
+        vertices = new Vector3[(xSize+1)*(ySize+1)];
+        for (int i = 0, y=0; y <= ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++,i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = xSize+1;
+        mesh.triangles = triangles;
+    }
+
+ 
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+        Gizmos.color=Color.black;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i],0.1f);
+        }
+    }
+
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
